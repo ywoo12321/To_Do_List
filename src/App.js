904d47e2349f34d0e28087916138074131c8f64f
ywoo12321 <ywoo12321@gmail.com>
2022-06-14 18:58:40 +0900

    import {MdCheckBox, MdCheckBoxOutlineBlank} from "react-icons/md"; //아이콘 불러오기     const { id, text, checked } = todo; //App.js 의 todos -> TodoList.js 로 불러오기 -> todos 객체의 id를 키값으로 설정해서 맵핑해 TodoItem에 'todo'로 불러오기 //불러온걸 선언             <div className = {`content ${checked ? ' checked' : '' }`}>                 {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}                 <div className = "text">{text}</div>             </div> //checked가 트루면 checkBox출력  false면 빈박스 출력

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { MdAddCircle } from 'react-icons/md';
+import Template from './components/Template';
+import TodoList from './components/TodoList';
 import './App.css';
-
-function App() {
+const App = () => { 
+  const [todos, setTodos] = useState([
+    {
+      id : 1,
+      text : "할일 1",
+      checked : true
+    },
+    {
+      id : 2,
+      text : "할일 2",
+      checked : false
+    },
+    {
+      id : 3,
+      text : "할일 3",
+      checked : true
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Template todoLength = {todos.length}>
+      <TodoList todos = {todos}/>
+      <div className = "add-todo-button">
+        <MdAddCircle/>
+      </div>
+    </Template>
   );
-}
-
+};
 export default App;
